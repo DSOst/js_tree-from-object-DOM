b413b2fed497a58ecfe6d09bff21126c6f6c29e2
Dmytro Ostapenko <dr.dm.ostapenko@gmail.com>
2025-07-06 12:31:04 +0300

    make build

---
+++ js_tree-from-object-DOM.c3c875ca.js
@@ -0,0 +1,2 @@
+"use strict";function _type_of(e){return e&&"undefined"!=typeof Symbol&&e.constructor===Symbol?"symbol":typeof e}var food={Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}},tree=document.querySelector("#tree");function createTree(e,t){var r=document.createElement("ul");for(var n in e.appendChild(r),t){var o=document.createElement("li");o.textContent=n,r.appendChild(o),"object"===_type_of(t[n])&&createTree(o,t[n])}}createTree(tree,food);
+//# sourceMappingURL=js_tree-from-object-DOM.c3c875ca.js.map
